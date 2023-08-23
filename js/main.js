9ceadfb4aa0f824fc49687e13d9c47b7ce7ac6b8
carlOzil <seifer01es@msn.com>
2023-08-23 19:47:32 +0200

    set files up and ready

---
+++ js/main.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    /// VARIABLES --->>>
+
+    /// EVENTOS --->>>
+
+    /// FUNCIONES --->>>
+
+    /// INVOCACIONES --->>>
+
+
+}) //////////LOAD//////////
